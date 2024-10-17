c216ebb9cd40407bffdbba2cdb8507a460ed172a
joeelfgrd <joelfigueiridomolares@gmail.com>
2024-10-17 09:07:26 +0200

    feat(Conexion.java): add getEpisodios,add getCast

--- src/main/java/edu/badpals/conexion/Conexion.java
+++ src/main/java/edu/badpals/conexion/Conexion.java
@@ -11,14 +11,8 @@ public class Conexion {
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.tvmaze.com/search/shows?q=" + serie)) // URL de la API
-                    .GET() // Método HTTP GET
-                    .build();
-
+                    .uri(URI.create("https://api.tvmaze.com/search/shows?q=" + serie)).GET().build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            int statusCode = response.statusCode();
-            System.out.println("Código de estado: " + statusCode);
             String responseBody = response.body();
             return responseBody;
 
@@ -27,4 +21,36 @@ public class Conexion {
         }
         return null;
     }
+    public static String getEpisodios(String idSerie) {
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.tvmaze.com/shows/"+idSerie+"/episodes")).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            return responseBody;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String getCast(String idSerie) {
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.tvmaze.com/shows/"+idSerie+"/cast")).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            return responseBody;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
